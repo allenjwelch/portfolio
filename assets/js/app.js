9cf4327715af08bb2ce88ba6f9cdaa818f7a919c
Allen Welch <allenjwelch@gmail.com>
2018-06-06 16:35:07 -0400

    removed center project cards and updated colors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 window.onload = function() {
   document.getElementById('contact-form').addEventListener('submit', function(event) {
       event.preventDefault();
-      console.log(this);
       // this.contact_number.value = Math.random() * 100000 | 0;
       emailjs.sendForm('gmail', 'portfolio_contact', this);
       console.log('message sent'); 
+      alert("Thank you for your message!"); 
+      
   });
 }
