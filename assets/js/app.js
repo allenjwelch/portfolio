5e4a094069fccafecf371393a3bddb451434d362
Allen Welch <allenjwelch@gmail.com>
2018-12-23 12:48:14 -0500

    initial branch commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,13 @@ window.onload = function() {
       event.preventDefault();
       // this.contact_number.value = Math.random() * 100000 | 0;
       emailjs.sendForm('gmail', 'portfolio_contact', this);
-      console.log('message sent'); 
-      alert("Thank you for your message!"); 
-      
+      console.log('message sent');
+      alert("Thank you for your message!");
+
   });
 }
+
+// Scroll Animation
+// $('#viewDesc').click(function() {
+//     $('html,body').animate({scrollTop: desc.offset().top -30 },'slow');
+// })
